118147b53330ac32f6c4b925ed156b92997ff70f
J. Eduardo V. Morango <edvmorango@gmail.com>
2025-03-30 23:41:18 -0300

    fold and vague

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,16 +2,11 @@ local g = vim.g -- a table to access global variables
 
 local o = vim.o
 
-
-
-
-
 g.filetype = true
 g.on = true
 g.syntax_on = true
 
 g.timeoutlen = 500
-
 
 o.autochdir = true
 
@@ -41,14 +36,21 @@ vim.wo.linebreak = true
 o.relativenumber = false
 o.textwidth = 120
 o.tabstop = 2
-o.clipboard = o.clipboard .. 'unnamedplus'        --copy paste
+o.clipboard = o.clipboard .. 'unnamedplus' --copy paste
 o.laststatus = 2
 o.viewoptions = 'folds,options,cursor,unix,slash' --UNIX better support
 o.encoding = 'utf-8'
-o.cmdheight = 2                                   -- "for echodoc
+o.cmdheight = 2 -- "for echodoc
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 
+o.foldenable = true
+o.foldlevel = 99
+o.foldmethod = 'expr'
+o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+o.foldtext = ''
+vim.opt.foldcolumn = '0'
+vim.opt.fillchars:append { fold = ' ' }
 
 function Map(mode, lhs, rhs, opts)
   local options = { noremap = true }
@@ -60,7 +62,6 @@ end
 
 -- eagle
 o.mousemoveevent = true
-
 
 Map('i', '<C-d>', '<Esc>')
 Map('n', '<C-d>', '<Esc>')
@@ -80,12 +81,9 @@ vim.opt.undofile = true
 
 vim.opt.jumpoptions = 'stack,view'
 
-
-
 vim.opt.termguicolors = true
 vim.opt.list = true
 vim.opt.listchars:append('space:·, nbsp:␣')
-
 
 --autosave
 g.auto_save = true
@@ -97,14 +95,24 @@ g.rooter_manual_only = true
 -- echodoc
 g.echodoc = { enable_at_startup = true, type = 'virtual' }
 
-
 -- float term
-g.floaterm_keyMap_new  = '<F7>'
+g.floaterm_keyMap_new = '<F7>'
 g.floaterm_keyMap_prev = '<C-w>'
 g.floaterm_keyMap_next = '<C-e>'
-g.floaterm_width       = 0.9
-g.floaterm_height      = 0.9
-
+g.floaterm_width = 0.9
+g.floaterm_height = 0.9
 
 -- subversive
 g.subversivePromptWithActualCommand = 0
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldmethod = 'expr'
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('LspDetach', { command = 'setl foldexpr<' })
--- nvim/plugin/theme-config.lua
+++ nvim/plugin/theme-config.lua
@@ -1,45 +1,46 @@
 local cmd = vim.cmd
 
-
 -- cmd 'colorscheme neodark'
 
-local colors = require("onenord.colors")
+local colors = require('onenord.colors')
 
 colors.load()
 
 local onenord = require('onenord')
 
-onenord.setup({
-  theme = 'dark',  -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
-  borders = true,  -- Split window borders
+onenord.setup {
+  theme = 'dark', -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+  borders = true, -- Split window borders
   fade_nc = false, -- Fade non-current windows, making them more distinguishable
   -- Style that is applied to various groups: see `highlight-args` for options
   styles = {
-    comments = "NONE",
-    strings = "NONE",
-    keywords = "NONE",
-    functions = "NONE",
-    variables = "NONE",
-    diagnostics = "undercurl",
+    comments = 'NONE',
+    strings = 'NONE',
+    keywords = 'NONE',
+    functions = 'NONE',
+    variables = 'NONE',
+    diagnostics = 'undercurl',
   },
   disable = {
     background = false, -- Disable setting the background color
     cursorline = false, -- Disable the cursorline
-    eob_lines = false,  -- Hide the end-of-buffer lines
+    eob_lines = false, -- Hide the end-of-buffer lines
   },
   -- Inverse highlight for different groups
   inverse = {
     match_paren = true,
   },
   custom_highlights = {}, -- Overwrite default highlight groups
-  custom_colors = {},     -- Overwrite default colors
-})
-
+  custom_colors = {}, -- Overwrite default colors
+}
 
 local evergarden = require('evergarden')
 
 evergarden.setup()
 
+local vague = require('vague')
+
+vague.setup()
 
 -- cmd('colorscheme onenord')
-cmd('colorscheme evergarden')
+cmd('colorscheme vague')
