e936a332fe51fe82ae0445826045bda16b2fe16a
J. Eduardo V. Morango <edvmorango@gmail.com>
2025-04-01 21:42:55 -0300

    fix

--- nvim/plugin/telescope-config.lua
+++ nvim/plugin/telescope-config.lua
@@ -2,7 +2,6 @@ local builtin = require('telescope.builtin')
 
 local telescope = require('telescope')
 
-
 local notify = require('notify')
 
 notify.setup()
@@ -19,8 +18,8 @@ nvim_web_devicons.setup {
     zsh = {
       icon = '',
       color = '#428850',
-      name = 'Zsh'
-    }
+      name = 'Zsh',
+    },
   },
   default = true,
 }
@@ -31,9 +30,9 @@ actions_preview.setup {
   telescope = {
     sorting_strategy = 'ascending',
     layout_strategy = 'vertical',
-    layout_config = TelescopeLayoutConfigVertical },
+    layout_config = TelescopeLayoutConfigVertical,
+  },
 }
-
 
 --require('telescope').load_extension('hoogle')
 telescope.load_extension('ui-select')
@@ -48,7 +47,6 @@ TelescopeLayoutConfigVertical = {
   horizontal = { width = 0.9 },
   width = resolve.resolve_width(0.9),
   height = resolve.resolve_height(0.99),
-  prompt_position = 'bottom'
 }
 
 TelescopeLayoutConfigHorizontal = {
@@ -57,7 +55,6 @@ TelescopeLayoutConfigHorizontal = {
   height = resolve.resolve_height(0.99),
   -- preview_width = resolve.resolve_width(0.65),
 }
-
 
 telescope.setup {
   defaults = {
@@ -77,14 +74,14 @@ telescope.setup {
       '--line-number',
       '--column',
       '--smart-case',
-      '--fixed-strings'
+      '--fixed-strings',
     },
-    path_display = { 'smart' }
+    path_display = { 'smart' },
   },
   pickers = {
     git_files = {
-      git_command = { 'git', 'ls-files', '--m' }
-    }
+      git_command = { 'git', 'ls-files', '--m' },
+    },
     -- Default configuration for builtin pickers goes here:
     -- picker_name = {
     --   picker_config_key = value,
@@ -92,7 +89,6 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
-
   },
   extensions = {
     -- Your extension configuration goes here:
@@ -103,13 +99,12 @@ telescope.setup {
     tele_tabby = { use_highlighter = true },
     lsp_handlers = {
       code_action = {
-        telescope = require('telescope.themes').get_dropdown({}),
+        telescope = require('telescope.themes').get_dropdown {},
       },
       rooter = {
         enable = true,
-        patterns = { '.git' }
-      }
-
+        patterns = { '.git' },
+      },
     },
     symbols = { sources = { 'math' } },
     glyph = {
@@ -119,12 +114,9 @@ telescope.setup {
         vim.fn.setreg('*', glyph.value)
         vim.api.nvim_put({ glyph.value }, 'c', false, true)
       end,
-    }
-  }
+    },
+  },
 }
-
-
-
 
 local search = require('search')
 
@@ -133,42 +125,41 @@ search.setup {
     {
       name = 'Changed Files',
       tele_func = function()
-        builtin.find_files({
+        builtin.find_files {
           find_command = { 'git', 'ls-files', '--modified', '--others', '--exclude-standard' },
-        })
-      end
-      ,
+        }
+      end,
       available = function()
         local current_dir = vim.fn.getcwd()
 
         local result = vim.fn.system('git -C ' .. current_dir .. ' rev-parse --is-inside-work-tree')
 
         return result:match('true') ~= nil
-      end
+      end,
     },
   },
   tabs = {
-    { 'Files',
-      builtin.find_files
-    },
-    { 'Grep',
+    {
+      'Grep',
       tele_func = function(opts)
-        if (opts ~= nil) then
-          builtin.live_grep({ cwd = opts.path, additional_args = { "--hidden", "--glob", "!.git/*" } })
+        vim.notify(vim.inspect(opts))
+
+        if opts.path ~= '' then
+          builtin.live_grep { cwd = opts.path, additional_args = { '--hidden', '--glob', '!.git/*' } }
         else
-          builtin.live_grep({ additional_args = { "--hidden", "--glob", "!.git/*" } })
+          builtin.live_grep { additional_args = { '--hidden', '--glob', '!.git/*' } }
         end
       end,
-      tele_opts = { path = '' }
-    }
-  }
-
+      tele_opts = { path = '' },
+    },
+    { 'Files', builtin.find_files },
+  },
 }
 
 local function telescope_server_ui()
   local current_file = vim.bo.filetype
 
-  if (current_file == 'scala' or current_file == 'sbt') then
+  if current_file == 'scala' or current_file == 'sbt' then
     require('telescope').extensions.metals.commands(TelescopeLayoutConfigVertical)
   end
 end
@@ -176,11 +167,7 @@ end
 vim.keymap.set('n', '<space>c', telescope_server_ui, { silent = true })
 
 vim.keymap.set('n', '<leader>g', function()
-  local current_dir = vim.fn.getcwd()
-
-  vim.cmd('lcd ' .. current_dir)
-
-  search.open({ tab_id = 3, tele_opts = { path = current_dir } })
+  search.open { tab_id = 3 }
 end, { silent = true })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { silent = true })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { silent = true })
@@ -188,28 +175,21 @@ vim.keymap.set('n', '<leader>qq', builtin.quickfix, { silent = true })
 vim.keymap.set('n', '<space>d', builtin.diagnostics, { silent = true })
 vim.keymap.set('n', '<space>a', actions_preview.code_actions, { silent = true })
 
-
-
-
-
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, { silent = true })
 vim.keymap.set('n', 'gy', builtin.lsp_type_definitions, { silent = true })
 vim.keymap.set('n', 'gi', builtin.lsp_implementations, { silent = true })
 vim.keymap.set('n', 'gr', builtin.lsp_references, { silent = true })
 vim.keymap.set('n', '<C-l>', builtin.lsp_dynamic_workspace_symbols, { silent = true })
-vim.keymap.set('n', '<C-s>', function() builtin.symbols { sources = { 'math' } } end, { silent = true })
+vim.keymap.set('n', '<C-s>', function()
+  builtin.symbols { sources = { 'math' } }
+end, { silent = true })
 
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { silent = true })
 vim.keymap.set('n', 'gs', vim.lsp.buf.document_symbol, { silent = true })
 vim.keymap.set('n', 'gw', vim.lsp.buf.workspace_symbol, { silent = true })
 
-vim.keymap.set('n', '<C-g>',
-  function()
-    local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+vim.keymap.set('n', '<C-g>', function()
+  local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
 
-    if git_root and git_root ~= '' then
-      vim.cmd('lcd ' .. git_root) -- Change to Git root
-    end
-
-    require('search').open({ tab_id = 3, tele_opts = { path = git_root } })
-  end, { silent = true })
+  require('search').open { tab_id = 1, tele_opts = { path = git_root } }
+end, { silent = true })
