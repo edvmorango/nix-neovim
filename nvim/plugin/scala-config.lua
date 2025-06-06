4de51ac6c35551a2059181b4826053d23db4a9f5
J. Eduardo V. Morango <edvmorango@gmail.com>
2025-06-06 15:58:54 -0300

    metals version

--- nvim/plugin/scala-config.lua
+++ nvim/plugin/scala-config.lua
@@ -10,7 +10,7 @@ metals_config.settings = {
 }
 
 metals_config.init_options.statusBarProvider = 'off'
-metals_config.settings.serverVersion = '1.5.3'
+metals_config.settings.serverVersion = '1.6.0'
 -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
 
 -- local capabilities = require('coq').lsp_ensure_capabilities(vim.lsp.protocol.make_client_capabilities())
