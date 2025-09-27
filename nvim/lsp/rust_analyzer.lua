0760606d547d21e11bfc143d7f4fbc3709f689f5
J. Eduardo V. Morango <edvmorango@gmail.com>
2025-09-27 17:23:15 -0300

    fix

--- nvim/lsp/rust_analyzer.lua
+++ nvim/lsp/rust_analyzer.lua
@@ -1,53 +1,9 @@
 return {
   autostart = true,
   cmd = { "rust-analyzer" },
-
   filetypes = { 'rust' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    local reused_dir = is_library(fname)
-    if reused_dir then
-      on_dir(reused_dir)
-      return
-    end
-
-    local cargo_crate_dir = vim.fs.root(fname, { 'Cargo.toml' })
-    local cargo_workspace_root
-
-    if cargo_crate_dir == nil then
-      on_dir(
-        vim.fs.root(fname, { 'rust-project.json' })
-        or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-      )
-      return
-    end
-
-    local cmd = {
-      'cargo',
-      'metadata',
-      '--no-deps',
-      '--format-version',
-      '1',
-      '--manifest-path',
-      cargo_crate_dir .. '/Cargo.toml',
-    }
-
-    vim.system(cmd, { text = true }, function(output)
-      if output.code == 0 then
-        if output.stdout then
-          local result = vim.json.decode(output.stdout)
-          if result['workspace_root'] then
-            cargo_workspace_root = vim.fs.normalize(result['workspace_root'])
-          end
-        end
-
-        on_dir(cargo_workspace_root or cargo_crate_dir)
-      else
-        vim.schedule(function()
-          vim.notify(('[rust_analyzer] cmd failed with code %d: %s\n%s'):format(output.code, cmd, output.stderr))
-        end)
-      end
-    end)
+  root_dir = function(fname)
+    return vim.fs.root(fname, { "Cargo.toml", "flake.nix", ".git" })
   end,
   settings = {
     ["rust-analyzer"] = {
